7272a9adc35f1ce6edde8832bac7d792c9a89276
bmusuko <bram.musuko@gmail.com>
2019-09-30 20:14:17 +0700

    refactor play turn

--- test.py
+++ test.py
@@ -6,32 +6,30 @@ board = Board()
 temp = 0
 
 while abs(temp) < 1000000:
-
-	print("Computer's Turn : ")
-	node_step = minimax.Node()
-	node_step.changeValue(-99999999,0)
-	for i in range(4,0,-1):
-		node_dummy = minimax.minimax(board,i,True,2**64 * -1, 2**64)
-		if (node_dummy.value > 1000000):
-			node_step.changeValue(node_dummy.value,node_dummy.step)
-			break
-		if (node_dummy.value > node_step.value):
-			node_step.changeValue(node_dummy.value,node_dummy.step)
+	if(board.turn == 0):
+		print("Computer's Turn : ")
+		node_step = minimax.Node()
+		node_step.changeValue(-99999999,0)
+		for i in range(4,0,-1):
+			node_dummy = minimax.minimax(board,i,True,2**64 * -1, 2**64)
+			if (node_dummy.value > 1000000):
+				node_step.changeValue(node_dummy.value,node_dummy.step)
+				break
+			if (node_dummy.value > node_step.value):
+				node_step.changeValue(node_dummy.value,node_dummy.step)
 	
+		print("Value node "+str(node_step.value))
+		print(node_step.step)
+		board.move(node_step.step)
 	# node_step = minimax.minimax(board,1,True,2**64 * -1, 2**64)
-	print("Value node "+str(node_step.value))
 	
-	print(node_step.step)
-	board.move(node_step.step)
-	temp = board.eval()
-	board.print()
-	if (abs(temp)>100000):
-		break
 	# x = randint(1,10)
 	# time.sleep(0.5)
-	x = int(input("masukkan move anda : "))
-	if not(board.move(x)):
-		print("incorrent move")
+	else:
+		x = int(input("masukkan move anda : "))
+		while(not(board.move(x))):
+			print("incorrent move")
+			x = int(input("masukkan move anda : "))
 	# if (abs(board.eval())>)
 	board.print()
 	temp = board.eval()
