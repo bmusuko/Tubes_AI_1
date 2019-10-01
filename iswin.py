e0c68803a785225e49a81d3b6462ac87318892e7
aryuuu <herppratama@gmail.com>
2019-10-01 21:40:19 +0700

    now is_win() returns a boolean and a list of tuple, denoting position of the connected four checkers

--- iswin.py
+++ iswin.py
@@ -2,8 +2,10 @@
 
 
 
-# this method returns true if the game is over
-# and false if it is not
+# this method returns a boolean and a list of tuple
+# if the game is over, this method returns True, and a list of tuple that denotes the connected four checkers
+# the tuple format is (row, column)
+# if the game is not over, this method returns False, and a list of dummy tuple
 # the game is over is there are four checkers in a row
 # either horizontally, vertically or diagonally.
 def is_win(self):
@@ -19,18 +21,20 @@ def is_win(self):
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(row, i) for i in range(column-3, column+1)]
 
 	# check vertically
 	for column in range(self.column):
 		connect_counter = 0
 		for row in range(self.row):
-			if(self.board[column][row] == checker):
+			if(self.board[row][column] == checker):
 				connect_counter += 1
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(i, column) for i in range(row-3,row+1)]
 
 
 	# check diagonally (top left to bottom right)
@@ -42,7 +46,8 @@ def is_win(self):
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(i, i) for i in range(point-3, point+1)]
 
 	for start_column in range(self.column-(4-1)):
 		connect_counter = 0 
@@ -52,28 +57,32 @@ def is_win(self):
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(i, i) for i in range(point-3, point+1)]
 
 	# now check diagonally bottom left to top right
 	for start_row in range(self.row-1, self.row-(4+1), -1):
 		connect_counter = 0
 		for point in range(start_row, -1, -1):
-			if(self.board[point][point] == checker):
+			if(self.board[point][self.column-point] == checker):
 				connect_counter += 1
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(i, self.column-i) for i in range(point+3, point-1, -1)]
+
 
 	for start_column in range(self.column-(4-1)):
 		connect_counter = 0
 		for point in range(start_column, self.column):
-			if(self.board[self.column-point-1][point] == checker):
+			if(self.board[point][self.column-point-1] == checker):
 				connect_counter += 1
 			else:
 				connect_counter = 0
 
-			return connect_counter == 4
+			if(connect_counter == 4):
+				return True, [(self.row-i, i) for i in range(point-3, point+1)]
 
 
-	return False
+	return False, [(i,i) for i in range(4)]
